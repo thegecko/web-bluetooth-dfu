4345068ac72c915e34556a9331096b414f180d6f
Rob Moran <rob.moran@arm.com>
2016-03-08 12:43:05 +0000

    Minor update to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
-    return gulp.src(['dist/*.js', '!dist/web-bluetooth*.js'])
+    return gulp.src(['dist/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
